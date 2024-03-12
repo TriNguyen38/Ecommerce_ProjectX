c7d6204905d95a6753bd3286d221bb979767d6f8
VŨ NGUYỄN MINH TIẾN <khoatien158@gmail.com>
2024-03-12 22:06:25 +0700

    router and model User

---
+++ Server/src/routers/user.js
@@ -0,0 +1,12 @@
+import Router from 'express';
+// import { register, login, getCurrent, refreshAccessToken, logout } from '../controllers/user';
+import verifyAccessToken from '../middlewares/verifyToken';
+
+const routerUser = Router();
+// routerUser.post("/createUser", register);
+// routerUser.post("/login", login);
+// routerUser.get('/current', verifyAccessToken, getCurrent);
+// routerUser.post('/refreshToken', refreshAccessToken);
+// routerUser.get('/logout', logout);
+
+export default routerUser;
