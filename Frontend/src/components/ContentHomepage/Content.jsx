8578501a68849e7aa3c3aed3583d2e4ba62d4d1c
TriNguyen38 <caotri3894@gmail.com>
2024-03-12 16:25:22 +0700

    update Slider

--- Frontend/src/components/ContentHomepage/Content.jsx
+++ Frontend/src/components/ContentHomepage/Content.jsx
@@ -10,24 +10,35 @@ const Content = () => {
     // Content Homepage
     <div className="px-32 py-2.5 flex flex-col w-full">
       {/* Top content */}
-      <div className="flex justify-around w-full items-start">
+      <div className="flex justify-evenly w-full items-start">
         {/* Left */}
-        <div className="flex flex-wrap text-xs">
+        <div className="flex text-xs text-wrap">
           <Navbar />
         </div>
         {/* Middle */}
-        <div className="flex flex-col items-center flex-wrap overflow-hidden ml-4 mr-4 border-2 rounded-lg">
-          <div className="  bg-slate-400 w-full flex flex-row overflow-hidden h-80">
-            <img src={img1} alt="Phone" />
-            <img src={img2} alt="Tablet" />
-            <img src={img3} alt="Laptop" />
+        <div className="flex flex-col items-center flex-wrap overflow-hidden ml-2 mr-2 w-3/5 border-2 rounded-lg">
+          <div className="  bg-slate-400 w-full flex flex-row overflow-hidden h-80 items-center">
+            {groupImg.map((imgItem, index) => (
+              <img key={index} src={imgItem} alt="Phone" />
+            ))}
+            <div className="flex">
+              <button className="absolute left-96 border-2 bg-emerald-500">LEFT</button>
+              <button className="absolute right-96 border-2 bg-emerald-500">RIGHT</button>
+            </div>
           </div>
-          <div className="py-7">
-              <a href="">infor</a>
+
+          <div className="py-7 w-full">
+            <a className="flex justify-around" href="">
+              <div>Phone</div>
+              <div>Tablet</div>
+              <div>Laptop</div>
+              <div>PC</div>
+              <div>SmartHome</div>
+            </a>
           </div>
         </div>
         {/* Right */}
-        <div className=" bg-green-300 h-[404px]">Banner</div>
+        <div className=" bg-green-300 h-[404px] w-52">Banner</div>
       </div>
 
       {/* Middle content */}
