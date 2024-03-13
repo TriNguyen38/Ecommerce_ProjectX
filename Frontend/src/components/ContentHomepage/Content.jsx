59b57e5bf486feb8259af6bf13f0f422d5011767
TriNguyen38 <caotri3894@gmail.com>
2024-03-13 22:27:09 +0700

    update Slider and Banner

--- Frontend/src/components/ContentHomepage/Content.jsx
+++ Frontend/src/components/ContentHomepage/Content.jsx
@@ -6,11 +6,9 @@ import Navbar from "..//Navbar/Navbar";
 import img1 from "..//..//imgs/img1.jpg";
 import img2 from "..//..//imgs/img2.jpg";
 import img3 from "..//..//imgs/img3.jpg";
-import banner1 from "..//..//imgs/banner1.webp"
-import banner2 from "..//..//imgs/banner2.webp"
-import banner3 from "..//..//imgs/banner3.webp"
-
-
+import banner1 from "..//..//imgs/banner1.webp";
+import banner2 from "..//..//imgs/banner2.webp";
+import banner3 from "..//..//imgs/banner3.webp";
 
 const Content = () => {
   const groupBanner = [banner1, banner2, banner3];
@@ -42,7 +40,7 @@ const Content = () => {
         </div>
         {/* Middle */}
         <div className="flex flex-col items-center flex-wrap overflow-hidden ml-2 mr-2 w-3/5 border-2 rounded-lg">
-          <div className="  bg-slate-400 w-full flex flex-row overflow-hidden h-80 items-center group">
+          <div className="  bg-slate-400 w-full flex flex-row overflow-hidden h-80 items-center group relative">
             {groupImg.map((imgItem, index) => (
               <img
                 style={{ transform: `translateX(-${slider * 100}%` }}
@@ -53,19 +51,24 @@ const Content = () => {
               />
             ))}
 
-            <div className="flex">
-              <button
-                className="hidden group-hover:block absolute left-96 bg-black/20 text-white text-2xl cursor-pointer rounded-full"
-                onClick={backSlide}
-              >
-                <BsChevronCompactLeft size={30} />
-              </button>
-              <button
-                className="hidden group-hover:block absolute right-96  bg-black/20 text-white text-2xl cursor-pointer rounded-full"
-                onClick={nextSlide}
-              >
-                <BsChevronCompactRight size={30} />
-              </button>
+            <div className="flex items-center">
+              <div>
+                <button
+                  className=" hidden group-hover:block absolute bg-black/20 left-2  text-white text-2xl cursor-pointer rounded-full"
+                  onClick={backSlide}
+                >
+                  <BsChevronCompactLeft size={30} />
+                </button>
+              </div>
+              <div>
+                <button
+                  className=" hidden group-hover:block absolute bg-black/20 right-2  text-white text-2xl cursor-pointer rounded-full"
+                  // hidden group-hover:block absolute bg-black/20
+                  onClick={nextSlide}
+                >
+                  <BsChevronCompactRight size={30} />
+                </button>
+              </div>
             </div>
           </div>
 
@@ -80,10 +83,14 @@ const Content = () => {
           </div>
         </div>
         {/* Right */}
-        <div className="h-[404px] w-52 flex flex-col justify-evenly">
-          {groupBanner.map((banner,index)=>(
-            <a className="mr-9  w-full" key={index} href="">
-              <img src={banner} alt="" />
+        <div className="h-[404px] flex flex-col w-1/4">
+          {groupBanner.map((banner, index) => (
+            <a className="h-full" key={index} href="">
+              <img
+                className="h-full w-full pb-1 rounded-lg"
+                src={banner}
+                alt=""
+              />
             </a>
           ))}
         </div>
