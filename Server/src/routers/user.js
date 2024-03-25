19279174efd5d4f02a5986ac00fa6e3fb6126a08
VŨ NGUYỄN MINH TIẾN <khoatien158@gmail.com>
2024-03-25 21:14:32 +0700

    BE forgot pass and change pass

--- Server/src/routers/user.js
+++ Server/src/routers/user.js
@@ -1,5 +1,5 @@
 import Router from 'express';
-import { register, login, getCurrent, refreshAccessToken, logout } from '../controllers/user';
+import { register, login, getCurrent, refreshAccessToken, logout, forgotPassword, resetPassword } from '../controllers/user';
 import verifyAccessToken from '../middlewares/verifyToken';
 
 const routerUser = Router();
@@ -8,5 +8,7 @@ routerUser.post("/login", login);
 routerUser.get('/current', verifyAccessToken, getCurrent);
 routerUser.post('/refreshToken', refreshAccessToken);
 routerUser.get('/logout', logout);
+routerUser.get('/forgotpassword', forgotPassword);
+routerUser.put('/resetpassword', resetPassword);
 
 export default routerUser;
